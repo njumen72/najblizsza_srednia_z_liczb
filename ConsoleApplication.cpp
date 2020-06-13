09e8faca21ed54edd69519b39a5b4e70d148963e
njumen72 <njumen@int.pl>
2020-06-14 00:18:12 +0200

    skalowalność

--- ConsoleApplication.cpp
+++ ConsoleApplication.cpp
@@ -6,10 +6,16 @@ const int n = 5;
 
 int main()
 {
-	cout << "Podaj " << n << " liczb.\n";
-	cout << "--------------\n";
+	int n;
+	cout << "Podaj ile liczb: ";
+	cin >> n;
+	cout << "----------------\n";
 
-	float liczba[n];
+	float* liczba;
+	float* p;
+	liczba = new float[n];
+	p = new float[n];
+
 	float suma = 0;
 
 	for (int i = 0; i < n; i++)
@@ -23,26 +29,21 @@ int main()
 
 	cout << "\nSrednia wynosi: " << m << endl << endl;
 
-	float p[n];
 	float s = 0;
 
 	for (int i = 0; i < n; i++)
 	{
 		if (liczba[i] > m)
-		{
 			p[i] = liczba[i] - m;
-		}
 		else if (liczba[i] < m)
-		{
 			p[i] = m - liczba[i];
-		}
+
 		else
 		{
 			cout << "Liczba najblizsza sredniej: " << m << endl;
 			return 0;
 		}
 		s += p[i];
-		//cout << p[i] << " " << liczba[i] << endl;
 	}
 	float x, y;
 
@@ -56,19 +57,13 @@ int main()
 			x = y = liczba[i];
 		}
 		else if (s == p[i])
-		{
 			y = liczba[i];
-		}
 	}
 
 	if (x == y)
-	{
 		cout << x << endl;
-	}
 	else
-	{
 		cout << x << " i " << y << endl;
-	}
 
 	cout << endl;
 	system("pause");
